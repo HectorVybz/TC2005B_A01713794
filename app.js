30975c3b8a03abe3bd1ddd28976dec7af4310445
HectorVybz <a01713794@tec.mx>
2026-02-19 12:32:49 -0600

    Lab 1,2,3,5

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("hola desde node!");
+
+const filesystem = require('fd');   
+
+filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+setTimeout(() => {
+    console.log("jojo te hackie");
+}, 15000);
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
