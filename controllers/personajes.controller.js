b4c2f354a49b61c99713bd6376b7eb94fe436610
HectorVybz <a01713794@tec.mx>
2026-03-03 12:45:30 -0600

    cambiando a rama develop

--- controllers/personajes.controller.js
+++ controllers/personajes.controller.js
@@ -1,17 +1,23 @@
-exports.get_add = ((request, response, next) => {
+const Personaje = require('../models/personaje.model');
+
+exports.get_add = (request, response, next) => {
     response.render('new');
-});
-exports.post_add = ((request, response, next) => {
-    console.log(request.body);
-    personajes.push(request.body);
+};
+
+exports.post_add = (request, response, next) => {
+    const personaje = new Personaje(request.body.nombre, 
+        request.body.descripcion, request.body.tipo, request.body.imagen);
+    personaje.save();
     response.redirect('/personajes');
-});
+};
 
-exports.get_old = ((request, response, next) => {
-    const path = request('path');
+exports.get_old = (request, response, next) => {
+    const path = require('path');
     response.sendFile(path.join(__dirname, '..', 'old_labs', 'index.html'));
-});
+};
 
-exports.get_list = ((request, response, next) => {
-    response.render('list', {personajes: personajes}); //Manda la respuesta
-});
+exports.get_list = (request, response, next) => {
+    console.log(request.session.username);
+    const personajes = Personaje.fetchAll();
+    response.render('list', {personajes: personajes}); 
+};
