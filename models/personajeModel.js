96c0e26d6f756c8c391b756af44112db4f7470ea
HectorVybz <a01713794@tec.mx>
2026-04-19 11:12:41 -0600

    lab 23 stored procedures

---
+++ models/personajeModel.js
@@ -0,0 +1,37 @@
+const db = require("../util/database");
+
+const getProcedureRows = ([rows, fieldData]) => {
+    return [rows[0] || [], fieldData];
+};
+
+module.exports = class PersonajeModel {
+    constructor(nombre, img, descripcion) {
+        this.nombre = nombre;
+        this.img = img;
+        this.descripcion = descripcion;
+    }
+
+    save() {
+        return db.execute(
+            "CALL sp_guardar_personaje(?, ?, ?, ?)",
+            [null, this.nombre, this.img, this.descripcion]
+        ).then(getProcedureRows);
+    }
+
+    static fetchAll() {
+        return db.execute("CALL sp_listar_personajes()")
+            .then(getProcedureRows);
+    }
+
+    static fetchOne(id) {
+        return db.execute("CALL sp_obtener_personaje(?)", [id])
+            .then(getProcedureRows);
+    }
+
+    static update(id, nombre, img, descripcion) {
+        return db.execute(
+            "CALL sp_guardar_personaje(?, ?, ?, ?)",
+            [id, nombre, img, descripcion]
+        ).then(getProcedureRows);
+    }
+};
